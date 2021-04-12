0a28e383550f50f7f61ab876b9cd6783b4d5ae0a
Lucas Mol Leite <lucas_mol@hotmail.com>
2021-04-11 23:26:22 -0300

    Lista de transações utilizando classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        body: ListaTransferencia(),
         appBar: AppBar(
           title: Text(
             "Referencias",
@@ -17,4 +16,44 @@ void main() {
       ),
     ),
   );
+}
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(100, 123)),
+        ItemTransferencia(Transferencia(22, 13)),
+        ItemTransferencia(Transferencia(0, 3)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  const ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.banco.toString()),
+        subtitle: Text(_transferencia.conta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double banco;
+  final int conta;
+
+  const Transferencia(this.banco, this.conta);
 }
